57680c90245a76a14e6a9dcec6f1e28c8662ba62
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2026-03-18 13:22:09 +0100

    V1.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,9 +6,19 @@ public class Main {
         String usuario = "RIBERA";
         String contraneña = "ribera";
 
-        try(Connection conn = DriverManager.getConnection(url,usuario,contraneña)){
-            String query = "select * " +
-                    "from empleado2";
+        try(Connection conn = DriverManager.getConnection(url,usuario,contraneña);
+            Statement stmt = conn.createStatement()){
+            String sql = "select * " +
+                    "from empleado2 " +
+                    "where MOD(ID,2)!=0";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                double salario = rs.getDouble("salario");
+                System.out.println("ID: "+id + " Nombre: " + nombre + " Salario: " + salario);
+            }
         }catch(SQLException e){
             System.out.println("error al mostrar tabla " + e.getMessage());
         }
